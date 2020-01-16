0307cce529212258e2dd5739594ef41a5ef49d8e
Steven Daniel <dstevie9292@gmail.com>
2020-01-16 19:50:14 +0000

    displined

--- lib/blocs/home_bloc/home_bloc.dart
+++ lib/blocs/home_bloc/home_bloc.dart
@@ -1,13 +1,23 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../app_bloc/app_bloc.dart';
+import '../app_bloc/app_states.dart';
 import 'home_events.dart';
 import 'home_states.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final AppBloc appBloc;
-  HomeBloc({@required this.appBloc}) : assert(appBloc != null);
+  StreamSubscription appSubscription;
+  HomeBloc({@required this.appBloc}) : assert(appBloc != null){
+    appSubscription = appBloc.listen((state){
+      if(state is AppLoadedState){
+        add(HomeLoadDataEvent(comic: (appBloc.state as AppLoadedState).data.comics, category: ));
+      }
+    });
+  }
   @override
   HomeState get initialState => HomeInitialState();
 
--- lib/blocs/home_bloc/home_events.dart
+++ lib/blocs/home_bloc/home_events.dart
@@ -1,9 +1,20 @@
 import 'package:equatable/equatable.dart';
 
+import '../../models/comic_category.dart';
+
 abstract class HomeEvent extends Equatable {
  const HomeEvent();
- @override
-  List<Object> get props => [];
+
 }
 
-class HomeLoadDataEvent extends HomeEvent {}
+class HomeLoa
+
+class HomeLoadDataEvent extends HomeEvent {
+  final List<Comic> comic;
+  final List<ComicCategory> category;
+
+  const HomeLoadDataEvent({@required this.comic, @required this.category});
+
+  @override
+  List<Object> get props => [comic, category];
+}
